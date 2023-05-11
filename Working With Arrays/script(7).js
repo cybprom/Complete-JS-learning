4ce9733b0e64f5288fabf9cb96862fb761e35bba
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-11 18:34:52 +0100

    Array from method edit

--- Working With Arrays/script(7).js
+++ Working With Arrays/script(7).js
@@ -545,4 +545,6 @@ console.log(z);
 labelBalance.addEventListener('click', () => {
     const movementsUI = Array.from(document.querySelectorAll('.movements__value'), el => Number(el.textContent.replace('€', '')));
     console.log(movementsUI);
+
+    const movementsUI2 = [...document.querySelectorAll('.movements__value')];
 });
