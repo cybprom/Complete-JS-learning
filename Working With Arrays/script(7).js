bc30b41e38fed78a5f6687361ce2c6d6030287ca
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-11 01:27:01 +0100

    Fill Method

--- Working With Arrays/script(7).js
+++ Working With Arrays/script(7).js
@@ -513,3 +513,25 @@ console.log(movements);
 
 
 */
+
+
+
+// More Ways Of Creating And Filling Arrays
+
+const arr =[1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+
+// When we pass in one argument it creates a new empty array with that length
+// Empty arrays + fill methods 
+const x = new Array(7);
+console.log(x);
+
+// FILL Method
+x.fill(1, 3, 5);  // it mutates the original array
+x.fill(1);  
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
