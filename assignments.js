07745353acbb148114cae4c4603be16300065db8
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-12 15:40:12 +0100

    coding challenge solution

--- assignments.js
+++ assignments.js
@@ -728,3 +728,54 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+
+
+const dogs = [
+    { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+    { weight: 8, curFood: 200, owners: ['Matilda'] },
+    { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+    { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+
+// 1. 
+// Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+dogs.forEach(dog => dog.recFood = Math.floor(dog.weight ** 0.75 * 28));
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(dogSarah);
+
+console.log(`Sarah's dog is eating too ${dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'}`);
+
+// 3.
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood > dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recFood).flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+// 4.
+// Matilda and Alice and Bob's dogs eat too much!
+// Sarah and John and Michael's dogs eat too little!
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+
+// 6.
+// Eating okay means: current > (recommended * 0.90) && current < (recommended * 1.10);
+console.log(dogs.some(dog => dog.curFood > (dog.recFood * 0.90) && dog.curFood < (dog.recFood * 1.10)));
+
+
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+console.log(dogs.filter(dog => dog.curFood > (dog.recFood * 0.90) && dog.curFood < (dog.recFood * 1.10)));
+
+
+// 8.
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
