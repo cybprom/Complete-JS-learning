753b90b3df72a2c3c4ebef6477bacc413218a5c1
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-15 01:26:13 +0100

    reminder operator

--- Numbers Dates And Timers/script.js
+++ Numbers Dates And Timers/script.js
@@ -300,6 +300,7 @@ console.log(Number.isInteger(23 / 0));
 
 ///////////////////
 // Math And Rounding
+/*
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
@@ -344,5 +345,24 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
 
+*/
+
 /////////////////
 // The Remainder Operator
+
+console.log(5 % 2);
+console.log(8 % 3);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener("click", () => {
+  [...document.querySelectorAll(".movements__row")].forEach((row, i) => {
+    // 0, 2, 4, 6, 8
+    if (i % 2 === 0) row.style.backgroundColor = "orange";
+    // number divisible by 3.. 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
+  });
+});
