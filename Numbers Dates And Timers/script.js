2f69fbc16262a7dcd3622ed5b3ce24d075d2808d
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-16 23:11:41 +0100

    timer implemented

--- Numbers Dates And Timers/script.js
+++ Numbers Dates And Timers/script.js
@@ -187,14 +187,43 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.floor(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    // When 0 sec., stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = "Log in to get started";
+      containerApp.style.opacity = 0;
+    }
+
+    // Decrese 1s
+    // time = time - 1;
+    time--;
+  };
+
+  // Set time to 5 mins
+  let time = 120;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = "100";
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = "100";
 
 btnLogin.addEventListener("click", function (e) {
   // Prevent form from submitting
@@ -243,6 +272,10 @@ btnLogin.addEventListener("click", function (e) {
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
 
+    // timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -272,6 +305,10 @@ btnTransfer.addEventListener("click", function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -293,6 +330,10 @@ btnLoan.addEventListener("click", function (e) {
 
       // Update UI
       updateUI(currentAccount);
+
+      // Reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
     }, 2500);
   }
   inputLoanAmount.value = "";
@@ -616,9 +657,13 @@ console.log("Waiting....");
 if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
 
 // setInterval
+/*
 setInterval(function () {
   const now = new Date();
   console.log(
     `The time is: ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
   );
 }, 1000);
+*/
+
+////////////////
