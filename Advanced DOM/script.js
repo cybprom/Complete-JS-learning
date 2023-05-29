927200de57419719e2842888999de0139aa03908
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-29 14:54:30 +0100

    remove event

--- Advanced DOM/script.js
+++ Advanced DOM/script.js
@@ -136,3 +136,15 @@ btnScrollTo.addEventListener("click", (e) => {
   // modern way
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+// Types of Event & Event Handler
+
+const h1 = document.querySelector("h1");
+
+const alertH1 = function (e) {
+  alert("addEventListener: Great! You are reading the heading :D");
+};
+
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
