a38205f88469ecce3a4bc055a119d8bcaa58c902
Akintunde Ilerioluwa <ilerioluwac@gmail.com>
2023-05-29 14:16:58 +0100

    implement smooth scroll

--- Advanced DOM/script.js
+++ Advanced DOM/script.js
@@ -33,6 +33,7 @@ document.addEventListener("keydown", function (e) {
 //////////////////////////////////////
 
 /// Creating and inserting element
+/*
 const header = document.querySelector(".header");
 
 const message = document.createElement("div");
@@ -99,3 +100,39 @@ logo.classList.add("c", "j");
 logo.classList.remove("c", "j");
 logo.classList.toggle("c");
 logo.classList.contains("c");
+
+*/
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    "height/width viewport",
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  //   window.scrollTo(
+  //     s1coords.left + window.pageXOffset,
+  //     s1coords.top + window.pageYOffset
+  //   );
+
+  // Old way of scrolling to
+  //   window.scrollTo({
+  //     left: s1coords.left + window.pageXOffset,
+  //     top: s1coords.top + window.pageYOffset,
+  //     behavior: "smooth",
+  //   });
+
+  // modern way
+  section1.scrollIntoView({ behavior: "smooth" });
+});
